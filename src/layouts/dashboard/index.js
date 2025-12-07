0a6b277bd1d1cd9b56bc799bd4129b324e67fd43
life2rich2025-commits <amurali123@gmail.com>
2025-12-07 15:25:45 +0530

    add funcation

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Grid from "@mui/material/Grid";
 import Switch from "@mui/material/Switch";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
+import axios from "axios";
 
 import MDBox from "components/MDBox";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -18,10 +19,14 @@ function Dashboard() {
   /* ------------------ DASHBOARD IMAGE STATE -------------------- */
   const [selectedImage, setSelectedImage] = useState(null);
   const [isImageActive, setIsImageActive] = useState(true);
-  const [dashboardImages, setDashboardImages] = useState([]);
+  const [dashboard, setDashboard] = useState({});
+  const [offer, setOffer] = useState([]);
+  const [dashboardImage, setDashboardImage] = useState([]);
+  const [imageTag, setImageTag] = useState("");
 
   /* ------------------ OFFER STATE -------------------- */
   const [offerTitle, setOfferTitle] = useState("");
+  const [offerDescription, setOfferDescription] = useState("");
   const [validDate, setValidDate] = useState("");
   const [isOfferActive, setIsOfferActive] = useState(true);
   const [offers, setOffers] = useState([]);
@@ -31,58 +36,100 @@ function Dashboard() {
     const file = e.target.files[0];
     if (file)
       setSelectedImage({
-        file,
+        file: file,
         url: URL.createObjectURL(file),
       });
   };
 
   /* ------------------ SAVE IMAGE -------------------- */
-  const saveDashboardImage = () => {
+  const saveDashboardImage = async () => {
     if (!selectedImage) return;
 
-    const newImage = {
-      url: selectedImage.url,
-      active: isImageActive,
-    };
+    console.log(imageTag);
 
-    setDashboardImages([...dashboardImages, newImage]);
+    const formData = new FormData();
+    formData.append("uploadImage", selectedImage.file);
+    formData.append("imagetag", imageTag);
+    formData.append("status", true);
 
-    // Reset
-    setSelectedImage(null);
-    setIsImageActive(true);
+    try {
+      const res = await axios.put("http://localhost:4000/api/dashboard/uploadImage", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+
+      console.log("Response:", res.data.response);
+      setDashboardImage(res.data.response);
+      alert("Image Uploaded Successfully!");
+    } catch (error) {
+      console.error(error);
+      alert("Upload failed!");
+    }
   };
 
   /* ------------------ SAVE OFFER -------------------- */
-  const saveOffer = () => {
+  const saveOffer = async () => {
     if (!offerTitle || !validDate) return;
 
     const newOffer = {
       title: offerTitle,
-      validDate,
-      active: isOfferActive,
+      description: offerDescription,
+      validTill: validDate,
     };
 
-    setOffers([...offers, newOffer]);
+    try {
+      const res = await axios.post("http://localhost:4000/api/dashboard/addOffer", newOffer, {
+        headers: { "Content-Type": "application/json" },
+      });
 
-    // Reset
-    setOfferTitle("");
-    setValidDate("");
-    setIsOfferActive(true);
+      console.log("Response:", res.data.response);
+      setOffer(res.data.response);
+      setOfferTitle("");
+      setOfferDescription("");
+      setValidDate("");
+      setIsOfferActive(true);
+      alert("Offer added successfully!");
+    } catch (error) {
+      console.error(error);
+      alert("Failed to add offer");
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardImages();
+  }, []);
+
+  const fetchDashboardImages = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/api/dashboard/home");
+      setDashboard(res.data.reponse);
+      setDashboardImage(res.data.reponse.imageUrl || []);
+      setOffer(res.data.reponse.offer || []);
+    } catch (err) {
+      console.log("Error fetching images:", err);
+    }
   };
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
-
       <MDBox py={3}>
         {/* ------------------ STAT CARDS ------------------ */}
         <Grid container spacing={3}>
           <Grid item xs={12} md={6} lg={3}>
-            <ComplexStatisticsCard color="dark" icon="weekend" title="Total Voucher" count={281} />
+            <ComplexStatisticsCard
+              icon="person_add"
+              title="Total Users"
+              count={dashboard?.totalUser?.length || 0}
+            />
           </Grid>
 
           <Grid item xs={12} md={6} lg={3}>
-            <ComplexStatisticsCard icon="leaderboard" title="Total Users" count="2,300" />
+            <ComplexStatisticsCard
+              color="dark"
+              icon="weekend"
+              title="Total Voucher"
+              count={dashboard?.totalVoucher?.length || 0}
+            />
           </Grid>
 
           <Grid item xs={12} md={6} lg={3}>
@@ -90,16 +137,16 @@ function Dashboard() {
               color="success"
               icon="store"
               title="Scratched Voucher"
-              count="34k"
+              count={dashboard?.scratedVoucher?.length || 0}
             />
           </Grid>
 
           <Grid item xs={12} md={6} lg={3}>
             <ComplexStatisticsCard
               color="primary"
-              icon="person_add"
+              icon="leaderboard"
               title="Total Amount Earn"
-              count="91"
+              count={dashboard?.totalAmountEarn || 0}
             />
           </Grid>
         </Grid>
@@ -109,7 +156,7 @@ function Dashboard() {
         ------------------------------------------------------------ */}
         <MDBox mt={4}>
           <Card style={{ padding: 20 }}>
-            <h3>Upload Dashboard Image</h3>
+            <h3>Banner Image</h3>
 
             {/* Upload */}
             <input
@@ -133,11 +180,24 @@ function Dashboard() {
               />
             )}
 
-            {/* Status Toggle */}
-            <MDBox mt={2} display="flex" alignItems="center" gap={2}>
-              <strong>Status:</strong>
-              <Switch checked={isImageActive} onChange={() => setIsImageActive(!isImageActive)} />
-              <strong>{isImageActive ? "Active" : "Inactive"}</strong>
+            <MDBox mt={2}>
+              <label>
+                <strong>Image Tag</strong>
+              </label>
+              <select
+                value={imageTag}
+                onChange={(e) => setImageTag(e.target.value)}
+                style={{
+                  width: "100%",
+                  padding: 10,
+                  borderRadius: 8,
+                  border: "1px solid #ddd",
+                  marginTop: 5,
+                }}
+              >
+                <option value="voucher">voucher</option>
+                <option value="dashboard">dashboard</option>
+              </select>
             </MDBox>
 
             {/* Save */}
@@ -154,7 +214,7 @@ function Dashboard() {
 
         {/* ------ Image Table -------- */}
         <MDBox mt={4}>
-          <DashboardImageTable images={dashboardImages} />
+          <DashboardImageTable images={dashboardImage} />
         </MDBox>
 
         {/* ------------------------------------------------------------
@@ -174,6 +234,26 @@ function Dashboard() {
                 value={offerTitle}
                 onChange={(e) => setOfferTitle(e.target.value)}
                 placeholder="Example: 20% OFF"
+                style={{
+                  width: "100%",
+                  padding: 10,
+                  borderRadius: 8,
+                  border: "1px solid #ddd",
+                  marginTop: 5,
+                }}
+              />
+            </MDBox>
+
+            {/* Offer Description */}
+            <MDBox mt={2}>
+              <label>
+                <strong>Offer Description</strong>
+              </label>
+              <input
+                type="text"
+                value={offerDescription}
+                onChange={(e) => setOfferDescription(e.target.value)}
+                placeholder=""
                 style={{
                   width: "100%",
                   padding: 10,
@@ -204,16 +284,16 @@ function Dashboard() {
             </MDBox>
 
             {/* Status Toggle */}
-            <MDBox mt={2} display="flex" alignItems="center" gap={2}>
+            {/* <MDBox mt={2} display="flex" alignItems="center" gap={2}>
               <strong>Status:</strong>
               <Switch checked={isOfferActive} onChange={() => setIsOfferActive(!isOfferActive)} />
               <strong>{isOfferActive ? "Active" : "Inactive"}</strong>
-            </MDBox>
+            </MDBox> */}
 
             {/* Save */}
             <Button
               variant="contained"
-              color="success"
+              color="primary"
               onClick={saveOffer}
               sx={{ mt: 3, color: "#fff" }}
             >
@@ -224,7 +304,7 @@ function Dashboard() {
 
         {/* ------ Offer Table -------- */}
         <MDBox mt={4}>
-          <OfferTable offers={offers} />
+          <OfferTable offers={offer} />
         </MDBox>
       </MDBox>
 
@@ -239,25 +319,34 @@ function Dashboard() {
 function DashboardImageTable({ images }) {
   return (
     <Card style={{ padding: 20 }}>
-      <h3>Dashboard Images</h3>
+      <h3>Banner Images</h3>
 
       <table style={{ width: "100%", marginTop: 20, borderCollapse: "collapse" }}>
         <thead>
           <tr style={{ background: "#f5f5f5" }}>
             <th style={{ padding: 10, border: "1px solid #ddd" }}>#</th>
             <th style={{ padding: 10, border: "1px solid #ddd" }}>Image</th>
+            <th style={{ padding: 10, border: "1px solid #ddd" }}>Image Name</th>
+            <th style={{ padding: 10, border: "1px solid #ddd" }}>Image Tag</th>
             <th style={{ padding: 10, border: "1px solid #ddd" }}>Status</th>
           </tr>
         </thead>
 
         <tbody>
           {images.map((img, idx) => (
-            <tr key={idx}>
+            <tr key={idx} style={{ textAlign: "center" }}>
               <td style={{ padding: 10, border: "1px solid #ddd" }}>{idx + 1}</td>
 
               <td style={{ padding: 10, border: "1px solid #ddd" }}>
-                <img src={img.url} alt="uploaded" style={{ width: 60, borderRadius: 8 }} />
+                <img
+                  src={"http://localhost:4000" + img.imageUrl}
+                  alt="uploaded"
+                  style={{ width: 60, borderRadius: 8 }}
+                />
               </td>
+              <td style={{ padding: 10, border: "1px solid #ddd" }}>{img.imagename}</td>
+
+              <td style={{ padding: 10, border: "1px solid #ddd" }}>{img.tag}</td>
 
               <td style={{ padding: 10, border: "1px solid #ddd" }}>
                 <span
@@ -295,7 +384,8 @@ function OfferTable({ offers }) {
         <thead>
           <tr style={{ background: "#f5f5f5" }}>
             <th style={{ padding: 10, border: "1px solid #ddd" }}>#</th>
-            <th style={{ padding: 10, border: "1px solid #ddd" }}>Offer</th>
+            <th style={{ padding: 10, border: "1px solid #ddd" }}>offerTitle</th>
+            <th style={{ padding: 10, border: "1px solid #ddd" }}>offerDescription</th>
             <th style={{ padding: 10, border: "1px solid #ddd" }}>Valid Date</th>
             <th style={{ padding: 10, border: "1px solid #ddd" }}>Status</th>
           </tr>
@@ -303,21 +393,30 @@ function OfferTable({ offers }) {
 
         <tbody>
           {offers.map((item, idx) => (
-            <tr key={idx}>
+            <tr key={idx} style={{ textAlign: "center" }}>
               <td style={{ padding: 10, border: "1px solid #ddd" }}>{idx + 1}</td>
               <td style={{ padding: 10, border: "1px solid #ddd" }}>{item.title}</td>
-              <td style={{ padding: 10, border: "1px solid #ddd" }}>{item.validDate}</td>
+              <td style={{ padding: 10, border: "1px solid #ddd" }}>{item.description}</td>
+              <td style={{ padding: 10, border: "1px solid #ddd" }}>
+                {new Date(item.validTill).toLocaleString("en-IN", {
+                  year: "numeric",
+                  month: "2-digit",
+                  day: "2-digit",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </td>
 
               <td style={{ padding: 10, border: "1px solid #ddd" }}>
                 <span
                   style={{
                     padding: "4px 10px",
-                    background: item.active ? "#4caf50" : "#f44336",
+                    background: item.status ? "#4caf50" : "#f44336",
                     color: "#fff",
                     borderRadius: 6,
                   }}
                 >
-                  {item.active ? "Active" : "Inactive"}
+                  {item.status ? "Active" : "Inactive"}
                 </span>
               </td>
             </tr>
